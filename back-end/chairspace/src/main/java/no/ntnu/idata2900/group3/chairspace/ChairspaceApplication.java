02ca8819272a08d24da8ac01ef7d98c0eeb7dbf7
Odin Lyngsgård <odin24@outlook.com>
2025-01-30 10:49:12 +0100

    Initial project commit

---
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/ChairspaceApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2900.group3.chairspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChairspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChairspaceApplication.class, args);
+	}
+
+}
---
+++ back-end/chairspace/src/test/java/no/ntnu/idata2900/group3/chairspace/ChairspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2900.group3.chairspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChairspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
