50d0190f74cdefb52574037364c41f2801715f1e
Odin Lyngsgård <odinlyngsgard01@gmail.com>
2025-02-24 01:02:58 +0100

    Added missing methods to area type

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/entity/AreaType.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/entity/AreaType.java
@@ -53,6 +53,15 @@ public class AreaType {
 		return description;
 	}
 
+	/**
+	 * Gets the areas of the area type.
+	 *
+	 * @return areas as set
+	 */
+	public Set<Area> getAreas() {
+		return areas;
+	}
+
 	/* ---- Setters ---- */
 
 	/**
@@ -61,6 +70,9 @@ public class AreaType {
 	 * @param name of the area type
 	 */
 	private void setName(String name) {
+		if (name == null || name.isEmpty()) {
+			throw new IllegalArgumentException("Name cannot be null or empty");
+		}
 		this.name = name;
 	}
 
@@ -70,6 +82,47 @@ public class AreaType {
 	 * @param description of area type as string.
 	 */
 	private void setDescription(String description) {
+		if (description == null || description.isEmpty()) {
+			throw new IllegalArgumentException("Description cannot be null or empty");
+		}
 		this.description = description;
+	}
+
+	/**
+	 * Sets the areas of the area type.
+	 *
+	 * @param areas as set
+	 */
+	private void setAreas(Set<Area> areas) {
+		if (areas == null) {
+			throw new IllegalArgumentException("Areas cannot be null");
+		}
+		for (Area area : areas) {
+			addArea(area);
+		}
+	}
+
+	/**
+	 * Adds an area to the area type.
+	 *
+	 * @param area to add
+	 */
+	public void addArea(Area area) {
+		if (area == null) {
+			throw new IllegalArgumentException("Area cannot be null");
+		}
+		areas.add(area);
+	}
+
+	/**
+	 * Removes an area from the area type.
+	 *
+	 * @param area to remove
+	 */
+	public void removeArea(Area area) {
+		if (area == null) {
+			throw new IllegalArgumentException("Area cannot be null");
+		}
+		areas.remove(area);
 	}
 }
