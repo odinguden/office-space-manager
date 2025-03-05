8d9ca48c2d1bf7057e5a8fecb3def7bab2750d5c
Odin Lyngsgård <odinlyngsgard01@gmail.com>
2025-03-05 14:03:17 +0100

    Added tests for reservation entity

---
+++ back-end/chairspace/src/test/java/no/ntnu/idata2900/group3/chairspace/entity/ReservationTest.java
@@ -0,0 +1,174 @@
+package no.ntnu.idata2900.group3.chairspace.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Odin Lyngsgård
+ * @version 0.1
+ * @since 0.1
+ * @see Reservation
+ */
+public class ReservationTest {
+	private static User admin;
+	private static User nonAdmin;
+	private static Area area;
+
+	@BeforeAll
+	static void initialize() {
+		admin = new User("Admin", "User", "Admin@Test", 12345678);
+		nonAdmin = new User("User", "User", "User@user", 3124325);
+		area = new Area.Builder(
+			"Area",
+			10,
+			new AreaType("Test type", "This is for testing")
+			).administrator(admin)
+			.build();
+	}
+
+	@Test
+	void testCreation() {
+		LocalDateTime start = LocalDateTime.now().plusDays(1);
+		LocalDateTime end = LocalDateTime.now().plusDays(1).plusHours(3);
+		String comment = "Writing tests";
+		Reservation reservation = new Reservation(
+			area,
+			nonAdmin,
+			start,
+			end,
+			comment
+			);
+		assertEquals(area, reservation.getArea(), "Area was not assigned correctly");
+		assertEquals(nonAdmin, reservation.getUser(), "User was not assigned correctly");
+		assertEquals(start, reservation.getStart(), "Start time was not assigned correctly");
+		assertEquals(end, reservation.getEnd(), "End time was not assigned correctly");
+		assertEquals(comment, reservation.getComment(), "comment was not assigned correctly");
+		area.removeReservation(reservation, admin);
+	}
+
+	@Test
+	void testThatNullAreaThrows() {
+		LocalDateTime start = LocalDateTime.now().plusDays(1);
+		LocalDateTime end = LocalDateTime.now().plusDays(1).plusHours(3);
+		String comment = "Writing tests";
+		assertThrows(
+			IllegalArgumentException.class,
+			() -> new Reservation(null, admin, start, end, comment)
+			);
+	}
+
+	@Test
+	void testThatNullUserThrows() {
+		LocalDateTime start = LocalDateTime.now().plusDays(1);
+		LocalDateTime end = LocalDateTime.now().plusDays(1).plusHours(3);
+		String comment = "Writing tests";
+		assertThrows(
+			IllegalArgumentException.class,
+			() -> new Reservation(area, null, start, end, comment)
+			);
+	}
+
+	@Test
+	void testThatNullStartThrows() {
+		LocalDateTime end = LocalDateTime.now().plusDays(1).plusHours(3);
+		String comment = "Writing tests";
+		assertThrows(
+			IllegalArgumentException.class,
+			() -> new Reservation(area, admin, null, end, comment)
+			);
+	}
+
+	@Test
+	void testThatNullEndThrows() {
+		LocalDateTime start = LocalDateTime.now().plusDays(1);
+		String comment = "Writing tests";
+		assertThrows(
+			IllegalArgumentException.class,
+			() -> new Reservation(area, admin, start, null, comment)
+			);
+	}
+
+	@Test
+	void testThatNullCommentThrows() {
+		LocalDateTime start = LocalDateTime.now().plusDays(1);
+		LocalDateTime end = LocalDateTime.now().plusDays(1).plusHours(3);
+		assertThrows(
+			IllegalArgumentException.class,
+			() -> new Reservation(area, admin, start, end, null)
+			);
+	}
+
+	@Test
+	void testThatBlankCommentThrows() {
+		LocalDateTime start = LocalDateTime.now().plusDays(1);
+		LocalDateTime end = LocalDateTime.now().plusDays(1).plusHours(3);
+		String comment = "";
+		assertThrows(
+			IllegalArgumentException.class,
+			() -> new Reservation(area, admin, start, end, comment)
+			);
+	}
+
+	@Test
+	void testThatCreatingOverlappingReservationsThrows() {
+		LocalDateTime start = LocalDateTime.now().plusDays(1);
+		LocalDateTime end = LocalDateTime.now().plusDays(1).plusHours(3);
+		String comment = "Writing tests";
+		Reservation reservation = new Reservation(
+			area,
+			nonAdmin,
+			start,
+			end,
+			comment
+			);
+		assertThrows(
+			IllegalStateException.class,
+			() -> new Reservation(
+				area,
+				nonAdmin,
+				start.plusHours(2),
+				end.plusHours(2),
+				comment
+				)
+			);
+	}
+
+	@Test
+	void testThatSetStartInPastThrows() {
+		LocalDateTime start = LocalDateTime.now().minusDays(1);
+		LocalDateTime end = LocalDateTime.now().minusDays(1).plusHours(3);
+		String comment = "Finding out what the fox says";
+		assertThrows(
+			IllegalStateException.class,
+			() -> new Reservation(
+				area,
+				nonAdmin,
+				start,
+				end,
+				comment
+				)
+			);
+	}
+
+	@Test
+	void testThatSetEndBeforeStartThrows() {
+		LocalDateTime start = LocalDateTime.now().plusDays(1);
+		LocalDateTime end = LocalDateTime.now().plusDays(1).minusHours(3);
+		String comment = "Finding out what the fox says";
+		assertThrows(
+			IllegalStateException.class,
+			() -> new Reservation(
+				area,
+				nonAdmin,
+				start.plusHours(2),
+				end.plusHours(2),
+				comment
+				)
+			);
+	}
+}
