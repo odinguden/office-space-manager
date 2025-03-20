ebd05a2ff91975f3f98dd29645ae34e342032b02
Odin Lyngsgård <odin24@outlook.com>
2025-03-20 15:26:06 +0100

    User tests work with new user class

--- back-end/chairspace/src/test/java/no/ntnu/idata2900/group3/chairspace/entity/UserTests.java
+++ back-end/chairspace/src/test/java/no/ntnu/idata2900/group3/chairspace/entity/UserTests.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import no.ntnu.idata2900.group3.chairspace.exceptions.InvalidArgumentCheckedException;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
