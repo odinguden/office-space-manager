0b7775cfb9ca137b4b107f5d4eab023294c82df2
Odin Lyngsgård <odin24@outlook.com>
2025-03-21 14:29:23 +0100

    Create AreaController.java

---
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/controllers/AreaController.java
@@ -0,0 +1,11 @@
+package no.ntnu.idata2900.group3.chairspace.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ */
+@RestController
+public class AreaController {
+	
+}
