0b0117a692c75dcfec8f6de31f9a1d24be06c4e1
Odin Lyngsgård <odin24@outlook.com>
2025-03-25 11:59:58 +0100

    Satisfying test coverage for pagination DTO

--- back-end/chairspace/src/test/java/no/ntnu/idata2900/group3/chairspace/dto/PaginationDtoTests.java
+++ back-end/chairspace/src/test/java/no/ntnu/idata2900/group3/chairspace/dto/PaginationDtoTests.java
@@ -1,8 +1,14 @@
 package no.ntnu.idata2900.group3.chairspace.dto;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import jakarta.validation.constraints.AssertTrue;
+import java.util.ArrayList;
+import no.ntnu.idata2900.group3.chairspace.entity.AreaFeature;
+import no.ntnu.idata2900.group3.chairspace.exceptions.InvalidArgumentCheckedException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests for the pagination dto.
@@ -10,9 +16,76 @@ import jakarta.validation.constraints.AssertTrue;
  * @see PaginationDTO
  */
 public class PaginationDTOTests {
+	private static ArrayList<AreaFeature> content;
+	private static String name = "Feature: ";
+	private static String description = "Description: ";
+
+	@BeforeAll
+	static void initialize() {
+		content = new ArrayList<>();
+
+		for (int i = 0; i < 100; i++) {
+			try {
+				content.add(
+					new AreaFeature(
+						name + i,
+						description + i
+						)
+				);
+			} catch (InvalidArgumentCheckedException e) {
+				fail("Failed to create Area feature with index" + i, e);
+			}
+		}
+	}
 
 	@Test
-	void assertTrueTest() {
+	void testConstructor() {
+		PaginationDTO<AreaFeature> paginationDto;
+		try {
+			paginationDto = new PaginationDTO<>(content, 20, 0);
+		} catch (InvalidArgumentCheckedException e) {
+			fail("Failed to create pagination", e);
+			return;
+		}
+		assertEquals(20, paginationDto.getPageContent().size(),
+			"Page content has the wrong number of items"
+		);
+		assertEquals(5, paginationDto.getNumberOfPages());
+	}
 
+	@Test
+	void testConstructorThrowsWhenNegativeCurrentPage() {
+		assertThrows(
+			InvalidArgumentCheckedException.class,
+			() -> new PaginationDTO<>(content, 10, -1)
+		);
+	}
+
+	@Test
+	void testConstructorThrowsWhenNegativeItemsPerPage() {
+		assertThrows(
+			IllegalArgumentException.class,
+			() -> new PaginationDTO<>(content, -10, 1)
+		);
+	}
+
+	@Test
+	void testPageNumberRoundUp() {
+		PaginationDTO<AreaFeature> paginationDto;
+		try {
+			paginationDto = new PaginationDTO<>(content, 24, 1);
+		} catch (InvalidArgumentCheckedException e) {
+			fail("Failed to create pagination", e);
+			return;
+		}
+		assertEquals(5, paginationDto.getNumberOfPages());
+	}
+
+	@Test
+	void testNullContent() {
+		assertThrows(
+			IllegalArgumentException.class,
+			() -> new PaginationDTO<AreaFeature>(null, 12, 3)
+		);
 	}
 }
