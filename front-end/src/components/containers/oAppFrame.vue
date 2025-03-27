34f99b1bd90532279a24ac0cd5d66f991a672cde
sigvebjo <sigve@bjorkedal.org>
2025-03-27 12:30:02 +0100

    Set up navigation drawer

--- front-end/src/components/containers/oAppFrame.vue
+++ front-end/src/components/containers/oAppFrame.vue
@@ -1,11 +1,20 @@
+<script setup>
+import { useDisplay } from 'vuetify';
+const { mobile } = useDisplay()
+
+// Whatever state the navdrawer should be in
+// Defaults to menu if desktop, but closed if mobile
+const navdrawerState = ref(mobile.value ? undefined : 0)
+</script>
+
 <template>
 	<o-appbar>
 		<template v-slot:prepend>
-			<o-navdrawer-controls />
+			<o-navdrawer-controls v-model="navdrawerState" />
 		</template>
 		<template v-slot:append>
 			<o-appbar-actions />
 		</template>
 	</o-appbar>
-	<o-navdrawer />
+	<o-navdrawer v-model="navdrawerState" />
 </template>
--- front-end/src/components/containers/oAppFrame/oNavdrawer.vue
+++ front-end/src/components/containers/oAppFrame/oNavdrawer.vue
@@ -1,5 +1,46 @@
+<script setup>
+const model = defineModel("modelValue", {
+	type: Number
+})
+
+// Figures out what window to show in the drawer
+const windowState = computed(() => {
+	if (model.value === undefined) {
+		// If the drawer is closed, return default
+		return 0
+	}
+
+	// Otherwise return the model value (0 or 1)
+	return model.value
+})
+
+// Figures out whether the drawer should be visible
+const visibleState = computed({
+	get() {
+		// If the modelvalue is undefined, the drawer is closed
+		return model.value !== undefined
+	},
+	set(newValue) {
+		if (newValue === false) {
+			// Sets the model's value to undefined (closed)
+			model.value = undefined
+		} else {
+			// Otherwise set to what window to show
+			model.value = windowState.value
+		}
+	}
+})
+</script>
+
 <template>
-	<v-navigation-drawer>
-		
+	<v-navigation-drawer v-model="visibleState">
+		<v-window v-model="windowState">
+			<v-window-item>
+				<o-navigation-drawer />
+			</v-window-item>
+			<v-window-item>
+				<o-search-drawer />
+			</v-window-item>
+		</v-window>
 	</v-navigation-drawer>
 </template>
