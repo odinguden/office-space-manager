dcd8ef748a088873749b13d46df94dcef5328736
sigvebjo <sigve@bjorkedal.org>
2025-03-28 12:11:18 +0100

    Added basic home screen containment

---
+++ front-end/src/components/containers/oContainedGrid.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+	maxWidth: {
+		type: String,
+		default: "328px"
+	}
+})
+</script>
+
+<template>
+	<section :style="{'--max-width': maxWidth}" class="contained-grid">
+		<slot />
+	</section>
+</template>
+
+<style scoped lang="scss">
+.contained-grid {
+	--max-width: 328px;
+	display: grid;
+	gap: 16px 24px;
+	grid-template-columns: repeat(auto-fill, minmax(var(--max-width), 1fr))
+}
+</style>
--- front-end/src/pages/index.vue
+++ front-end/src/pages/index.vue
@@ -1,7 +1,42 @@
+<script setup>
+const defaultContent = Array(12)
+
+const sections = [
+	{
+		title: "Favorites",
+		content: defaultContent,
+		link: "/"
+	},
+	{
+		title: "Popular",
+		content: defaultContent,
+		link: "/"
+	},
+	{
+		title: "Recent",
+		content: defaultContent,
+		link: "/"
+	}
+]
+</script>
+
 <template>
-	<div></div>
+	<div>
+		<section v-for="section in sections">
+			<div class="section-header">
+				<h1>{{ section.title }}</h1>
+				<v-spacer />
+				<router-link :to="section.link">See more...</router-link>
+			</div>
+			<o-contained-grid>
+				<o-space-card v-for="_ in section.content" />
+			</o-contained-grid>
+		</section>
+	</div>
 </template>
 
-<script setup>
-	//
-</script>
+<style scoped lang="scss">
+.section-header {
+	display: flex;
+}
+</style>
