29cbded7bb803b7f8e901797f63ffea89169a359
sigvebjo <sigve@bjorkedal.org>
2025-03-28 12:43:42 +0100

    Home screen now primarily uses horizontal scrolling

--- front-end/src/pages/index.vue
+++ front-end/src/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-const defaultContent = Array(12)
+const defaultContent = Array(6)
 
 const sections = [
 	{
@@ -21,16 +21,17 @@ const sections = [
 </script>
 
 <template>
-	<div>
-		<section v-for="section in sections">
-			<div class="section-header">
-				<h1>{{ section.title }}</h1>
-				<v-spacer />
-				<router-link :to="section.link">See more...</router-link>
+	<div class="home-section-container">
+		<section v-for="section in sections" class="card-section mt-3">
+			<h1>{{ section.title }}</h1>
+			<div class="card-container-wrapper">
+				<div class="card-container">
+					<o-space-card v-for="_ in section.content" />
+					<o-flat-card class="see-more-card" :to="section.link">
+						<h1><router-link :to="section.link">See more...</router-link></h1>
+					</o-flat-card>
+				</div>
 			</div>
-			<o-contained-grid>
-				<o-space-card v-for="_ in section.content" />
-			</o-contained-grid>
 		</section>
 	</div>
 </template>
@@ -38,5 +39,30 @@ const sections = [
 <style scoped lang="scss">
 .section-header {
 	display: flex;
+}
+
+.home-section-container {
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+}
+
+.card-container {
+	position: relative;
+	display: flex;
+	overflow-x: auto;
+	gap: 16px 24px;
+	padding: 8px 0 16px 0;
+
+
+	> * {
+		flex: 0 0 328px;
+	}
+}
+
+.see-more-card {
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 </style>
