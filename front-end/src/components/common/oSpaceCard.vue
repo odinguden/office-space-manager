52fe37eb11cad25f77ded75201261acd5f65fd50
sigvebjo <sigve@bjorkedal.org>
2025-03-28 12:10:46 +0100

    Created space card components

---
+++ front-end/src/components/common/oSpaceCard.vue
@@ -0,0 +1,69 @@
+<script setup>
+const isFavorite = ref(Math.random() > 0.7)
+</script>
+
+<template>
+	<v-card
+		class="space-card"
+		elevation="0"
+		tile
+	>
+		<v-icon>mdi-desk</v-icon>
+		<div class="card-header">
+			<h1>A123</h1>
+			<v-btn
+				icon
+				flat
+				variant="text"
+				density="comfortable"
+				:color="isFavorite ? 'warning' : ''"
+				@click="isFavorite = !isFavorite"
+			>
+				<v-icon class="favorite" :class="{'is-favorite': isFavorite}">
+					mdi-star{{ isFavorite ? '' : '-outline' }}
+				</v-icon>
+			</v-btn>
+		</div>
+		<div class="card-content">
+			<p>Something about this room</p>
+		</div>
+		<div class="card-extras">
+			<o-space-extras />
+		</div>
+		<div class="card-timeline">
+			<o-timeline/>
+		</div>
+	</v-card>
+</template>
+
+<style scoped lang="scss">
+.space-card {
+	border: 1px solid rgba(var(--v-border-color), var(--v-border-opacity));
+	padding: 16px 24px;
+	border-radius: 4px;
+	display: grid;
+	grid-template-columns: auto 1fr;
+	gap: 16px;
+	align-items: center;
+
+	.card-header {
+		display: grid;
+		grid-template-columns: 1fr auto;
+		gap: 16px;
+		align-items: center;
+	}
+
+	.card-content {
+		grid-column: 2;
+	}
+
+	.card-extras {
+		grid-column: 2;
+	}
+
+	.card-timeline {
+		grid-column: 1 / span 2;
+		height: 24px;
+	}
+}
+</style>
