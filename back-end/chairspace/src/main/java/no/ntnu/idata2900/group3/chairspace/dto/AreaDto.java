50ab750264e768cfd06902635a7de21fd51060f0
Odin Lyngsgård <odinlyngsgard01@gmail.com>
2025-04-01 12:12:51 +0200

    Documented and finished area dto

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/dto/AreaDto.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/dto/AreaDto.java
@@ -9,7 +9,7 @@ import no.ntnu.idata2900.group3.chairspace.entity.Area;
 import no.ntnu.idata2900.group3.chairspace.entity.AreaFeature;
 import no.ntnu.idata2900.group3.chairspace.entity.AreaType;
 import no.ntnu.idata2900.group3.chairspace.entity.User;
-import org.apache.commons.lang3.NotImplementedException;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * A data transfer object used for creation and retrieval of area objects.
@@ -18,7 +18,7 @@ import org.apache.commons.lang3.NotImplementedException;
 public class AreaDto {
 	private UUID id;
 	private List<UUID> administrators;
-	private UUID superArea;
+	private List<SimpleSuperAreaDTO> superAreas;
 	private AreaType areaType;
 	private List<AreaFeature> areaFeatures;
 	private int capacity;
@@ -38,13 +38,10 @@ public class AreaDto {
 		this.name = area.getName();
 		this.description = area.getDescription();
 		this.reservable = area.isReservable();
-		if (area.getSuperArea() != null) {
-			this.superArea = area.getSuperArea().getId();
-		} else {
-			this.superArea = null;
-		}
 		this.areaType = area.getAreaType();
 		this.administrators = new ArrayList<>();
+
+		setSuperAreas(area);
 
 		Set<User> admins = area.getAdministrators();
 		for (User user : admins) {
@@ -59,59 +56,126 @@ public class AreaDto {
 
 	}
 
+	private void setSuperAreas(Area area) {
+		superAreas = new ArrayList<>();
+
+		Area nextSuper = area.getSuperArea();
+
+		while (nextSuper != null) {
+			superAreas.add(
+				new SimpleSuperAreaDTO(nextSuper)
+			);
+			nextSuper = nextSuper.getSuperArea();
+		}
+	}
+
 	/* ---- Getters ---- */
 
 	/**
 	 * Returns the id of the area this DTO represents.
 	 *
-	 * @return 
+	 * @return uuid
 	 */
 	public UUID getId() {
 		return id;
 	}
 
+	/**
+	 * Returns a list containing the UUID's of the users administering the area this dto represents.
+	 *
+	 * @return list of UUID's
+	 */
 	public List<UUID> getAdministrators() {
 		return administrators;
 	}
 
-	public UUID getSuperArea() {
-		return superArea;
+	/**
+	 * Returns a list containing simpleSuperAreaDto's for all the super areas of this areaDTO.
+	 *
+	 * @return List containing SimpleSuperAreaDTO's
+	 */
+	public List<SimpleSuperAreaDTO> getSuperAreas() {
+		return superAreas;
 	}
 
+	/**
+	 * Returns area type of area.
+	 * Will return full area type object containing name and description.
+	 *
+	 * @return area type
+	 */
 	public AreaType getAreaType() {
 		return areaType;
 	}
 
+	/**
+	 * Returns list of area features of the area this DTO represents.
+	 * Returns full area feature object containing name and description.
+	 *
+	 * @return list of area features
+	 */
 	public List<AreaFeature> getAreaFeatures() {
 		return areaFeatures;
 	}
 
+	/**
+	 * Returns the capacity of the area this DTO represents.
+	 *
+	 * @return capacity
+	 */
 	public int getCapacity() {
 		return capacity;
 	}
 
+	/**
+	 * Returns calendar link of the area this area represents.
+	 * Will be null if no calendar link exits.
+	 *
+	 * @return calendar link.
+	 */
 	public String getCalendarLink() {
 		return calendarLink;
 	}
 
+	/**
+	 * Returns the name string of the area this DTO represents.
+	 *
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Returns the description of the area this DTO represents.
+	 *
+	 * @return description
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Returns the reservable status of this area.
+	 *
+	 * @return reservable status
+	 */
 	public boolean isReservable() {
 		return reservable;
 	}
 
-	/**
-	 * TODO:
-	 *
-	 * @return Area based on this object
-	 */
-	public Area buildArea() {
-		throw new NotImplementedException();
+	private class SimpleSuperAreaDTO {
+		@JsonProperty
+		private UUID id;
+		@JsonProperty
+		private String name;
+		@JsonProperty
+		private String areaType;
+
+		public SimpleSuperAreaDTO(Area superArea) {
+			this.id = superArea.getId();
+			this.name = superArea.getName();
+			this.areaType = superArea.getAreaType().getId();
+		}
 	}
 }
