9b230059f41a182d8d4e56889bd6ed2d1aa826f1
sigvebjo <sigve@bjorkedal.org>
2025-04-03 10:12:04 +0200

    Enabled navigation to bookings screen

---
+++ front-end/src/pages/bookings.vue
@@ -0,0 +1,9 @@
+<template>
+	<o-contained-grid>
+		<o-space-card v-for="_ in Array(4)">
+			<template #actions>
+				<o-space-booked-actions />
+			</template>
+		</o-space-card>
+	</o-contained-grid>
+</template>
