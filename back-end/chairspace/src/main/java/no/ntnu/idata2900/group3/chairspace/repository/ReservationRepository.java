1104fd3e02290204f73bf4cdc327c063e4ca8629
Odin Lyngsgård <odin24@outlook.com>
2025-04-04 15:41:29 +0200

    Added method to get reservation relation to a area to ReservationRepository

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/repository/ReservationRepository.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/repository/ReservationRepository.java
@@ -1,7 +1,9 @@
 package no.ntnu.idata2900.group3.chairspace.repository;
 
+import java.util.List;
 import java.util.UUID;
 import no.ntnu.idata2900.group3.chairspace.entity.Reservation;
+import org.hibernate.annotations.processing.SQL;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,14 @@ import org.springframework.stereotype.Repository;
  * Repository for the Reservation entity.
  */
 @Repository
-public interface ReservationRepository extends CrudRepository<Reservation, UUID> {}
+public interface ReservationRepository extends CrudRepository<Reservation, UUID> {
+
+	/**
+	 * Finds all reservations for a given area.
+	 *
+	 * @param area the area to find reservations for
+	 * @return a list of reservations for the given area
+	 */
+	@SQL("SELECT * FROM reservation WHERE area = ?1")
+	public List<Reservation> findByArea(Reservation area);
+}
