8765c56f1e558ae48ed65c7fc22a2b8db1ba4cbe
Odin Lyngsgård <odinlyngsgard01@gmail.com>
2025-04-08 14:02:31 +0200

    Added static methods to create errors with predefined messages for area and area feature to elementNotFoundException

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/exceptions/ElementNotFoundException.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/exceptions/ElementNotFoundException.java
@@ -1,5 +1,7 @@
 package no.ntnu.idata2900.group3.chairspace.exceptions;
 
+import java.util.UUID;
+
 /**
  * To be used by service classes when an element can not be found in the database.
  * Extends Exception
@@ -14,5 +16,31 @@ public class ElementNotFoundException extends Exception {
 	 */
 	public ElementNotFoundException(String message) {
 		super(message);
+	}
+
+	/**
+	 * Creates new instance of ElementNotFoundException with predefined text
+	 * for if an area was not found.
+	 *
+	 * @param areaId id of the area that could not be found
+	 * @return new ElementNotFoundException with matching error message
+	 */
+	public static ElementNotFoundException areaNotFoundException(UUID areaId) {
+		return new ElementNotFoundException(
+			"Could not find area with id: " + areaId
+		);
+	}
+
+	/**
+	 * Creates new instance of ElementNotFoundException with predefined text
+	 * for if a area feature was not found.
+	 *
+	 * @param featureId id of the area feature that could not be found
+	 * @return new ElementNotFoundException with matching error message
+	 */
+	public static ElementNotFoundException areaFeatureNotFoundException(String featureId) {
+		return new ElementNotFoundException(
+			"Could not find area feature with name: " + featureId
+		);
 	}
 }
