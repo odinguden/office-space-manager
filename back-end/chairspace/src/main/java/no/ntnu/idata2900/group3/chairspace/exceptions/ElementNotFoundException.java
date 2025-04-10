2fb58a6fff9c8171c45f892abd3e0dab88a9c03f
Odin Lyngsgård <odinlyngsgard01@gmail.com>
2025-04-10 11:23:25 +0200

    Changed methods to generate errorsto be public static final variables in ElementNotFoundException

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/exceptions/ElementNotFoundException.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/exceptions/ElementNotFoundException.java
@@ -1,12 +1,20 @@
 package no.ntnu.idata2900.group3.chairspace.exceptions;
 
-import java.util.UUID;
-
 /**
  * To be used by service classes when an element can not be found in the database.
  * Extends Exception
  */
 public final class ElementNotFoundException extends Exception {
+	public static final ElementNotFoundException entityNotFoundException =
+		new ElementNotFoundException("Could not find item");
+	public static final ElementNotFoundException areaNotFoundException =
+		new ElementNotFoundException("Could not find area");
+	public static final ElementNotFoundException areaFeatureNotFoundException =
+		new ElementNotFoundException("Could not find area feature");
+	public static final ElementNotFoundException areaTypeNotFoundException =
+		new ElementNotFoundException("Could not find area type");
+	public static final ElementNotFoundException userNotFoundException =
+		new ElementNotFoundException("Could not find user");
 
 	/**
 	 * Constructs a new instance of element not found exception.
@@ -16,65 +24,5 @@ public final class ElementNotFoundException extends Exception {
 	 */
 	private ElementNotFoundException(String message) {
 		super(message);
-	}
-
-	/**
-	 * Creates new instance of ElementNotFoundException with predefined text for if a
-	 * generic entity could not be found.
-	 *
-	 * @return new ElementCouldNotBeFound instance with matching message
-	 */
-	public static ElementNotFoundException entityNotFoundException() {
-		return new ElementNotFoundException(
-			"Could not find item"
-		);
-	}
-
-	/**
-	 * Creates new instance of ElementNotFoundException with predefined text
-	 * for if an area was not found.
-	 *
-	 * @return new ElementNotFoundException with matching error message
-	 */
-	public static ElementNotFoundException areaNotFoundException() {
-		return new ElementNotFoundException(
-			"Could not find area"
-		);
-	}
-
-	/**
-	 * Creates new instance of ElementNotFoundException with predefined text
-	 * for if a area feature was not found.
-	 *
-	 * @return new ElementNotFoundException with matching error message
-	 */
-	public static ElementNotFoundException areaFeatureNotFoundException() {
-		return new ElementNotFoundException(
-			"Could not find area feature"
-		);
-	}
-
-	/**
-	 * Creates new instance of ElementNotFoundException with predefined text
-	 * for if a area type is not found in the database.
-	 *
-	 * @return new ElementNotFoundException with matching error message
-	 */
-	public static ElementNotFoundException areaTypeNotFoundException() {
-		return new ElementNotFoundException(
-			"Could not find area type"
-		);
-	}
-
-	/**
-	 * Creates new instance of ElementNotFoundException with predefined text
-	 * for if a user is not found in the database.
-	 *
-	 * @return new ElementNotFoundException with matching error message
-	 */
-	public static ElementNotFoundException userNotFoundException() {
-		return new ElementNotFoundException(
-			"Could not find user"
-		);
 	}
 }
