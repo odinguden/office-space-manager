2f5f296dc290b0be8ed3be072bb438b427daab15
sigvebjo <sigve@bjorkedal.org>
2025-04-10 09:29:19 +0200

    Documented existing endpoints

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/controllers/ReservationController.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/controllers/ReservationController.java
@@ -1,24 +1,16 @@
 package no.ntnu.idata2900.group3.chairspace.controllers;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import no.ntnu.idata2900.group3.chairspace.dto.reservation.ReservationCreationDto;
 import no.ntnu.idata2900.group3.chairspace.dto.reservation.ReservationDto;
-import no.ntnu.idata2900.group3.chairspace.entity.Area;
-import no.ntnu.idata2900.group3.chairspace.entity.Reservation;
-import no.ntnu.idata2900.group3.chairspace.entity.User;
 import no.ntnu.idata2900.group3.chairspace.exceptions.InvalidArgumentCheckedException;
 import no.ntnu.idata2900.group3.chairspace.exceptions.NotReservableException;
 import no.ntnu.idata2900.group3.chairspace.exceptions.ReservedException;
-import no.ntnu.idata2900.group3.chairspace.repository.AreaRepository;
-import no.ntnu.idata2900.group3.chairspace.repository.ReservationRepository;
-import no.ntnu.idata2900.group3.chairspace.repository.UserRepository;
 import no.ntnu.idata2900.group3.chairspace.service.ReservationService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,16 +19,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
-
 
 /**
  * Reservation controller.
@@ -55,6 +42,12 @@ public class ReservationController extends AbstractPermissionManager {
 	 * @return 200 OK with the found reservation
 	 */
 	@GetMapping("/{id}")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "Found a reservation with the given id"
+			)
+	})
 	public ResponseEntity<ReservationDto> getReservation(@PathVariable UUID id) {
 		super.hasPermissionToGet();
 		ReservationDto reservationDto = reservationService.getReservationById(id);
@@ -70,6 +63,12 @@ public class ReservationController extends AbstractPermissionManager {
 	 * @return 200 OK with a list of reservations for the given time period
 	 */
 	@GetMapping("/area/{areaId}")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "Collected a list of all reservations within the given timeframe"
+			)
+	})
 	public ResponseEntity<List<ReservationDto>> getReservationsInTimeFrame(
 		@PathVariable UUID areaId,
 		@RequestParam LocalDateTime start,
@@ -86,9 +85,37 @@ public class ReservationController extends AbstractPermissionManager {
 			HttpStatus.OK
 		);
 	}
-	
 
+	/**
+	 * Attempts to create a new reservation based on a DTO.
+	 *
+	 * @param creationDto a simple DTO containing information about the reservation to be created
+	 * @return 201 CREATED if the reservation was created
+	 * @throws ResponseStatusException 400 BAD REQUEST if the creation DTO contains invalid values
+	 * @throws ResponseStatusException 400 BAD REQUEST if the reservation to be created is for an
+	 *     area that cannot be reserved
+	 * @throws ResponseStatusException 409 CONFLICT if the reservation to be created overlaps
+	 *     with an existing reservation
+	 */
 	@PostMapping("")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "201",
+			description = "Succesfully created the reservation"
+			),
+		@ApiResponse(
+			responseCode = "400",
+			description = "If the creation parameters contain invalid values"
+			),
+		@ApiResponse(
+			responseCode = "400",
+			description = "If the area to be reserved does not allow reservations"
+			),
+		@ApiResponse(
+			responseCode = "409",
+			description = "If the reservation would overlap with an existing one"
+			)
+	})
 	public ResponseEntity<String> postMethodName(@RequestBody ReservationCreationDto creationDto) {
 		super.hasPermissionToPost();
 
@@ -103,6 +130,23 @@ public class ReservationController extends AbstractPermissionManager {
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
+	/**
+	 * Attempts to delete the reservation with the given ID.
+	 *
+	 * @param id the id of the reservation to be deleted
+	 * @return 204 NO CONTENT if the reservation was deleted
+	 * @throws ResponseStatusException 404 NOT FOUND if the id does not exist in the database
+	 */
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "204",
+			description = "Successfully deleted the reservation"
+			),
+		@ApiResponse(
+			responseCode = "404",
+			description = "If the reservation to be deleted does not exist"
+			)
+	})
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteReservation(@PathVariable UUID id) {
 		// FIXME: Ensure that reservations can only be deleted by owning or administrating users
