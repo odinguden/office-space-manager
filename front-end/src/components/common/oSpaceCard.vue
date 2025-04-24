feaedf98495a9ff30b65ff318c7a9315a47fcdaf
sigvebjo <sigve@bjorkedal.org>
2025-04-24 09:33:54 +0200

    Setup front end page to use real areas

--- front-end/src/components/common/oSpaceCard.vue
+++ front-end/src/components/common/oSpaceCard.vue
@@ -1,5 +1,7 @@
 <script setup>
-const isFavorite = ref(Math.random() > 0.7)
+const props = defineProps({
+	area: Object
+})
 </script>
 
 <template>
@@ -10,22 +12,10 @@ const isFavorite = ref(Math.random() > 0.7)
 	>
 		<v-icon>mdi-desk</v-icon>
 		<div class="card-header">
-			<h1><router-link to="/room/0">A123</router-link></h1>
-			<v-btn
-				icon
-				flat
-				variant="text"
-				density="comfortable"
-				:color="isFavorite ? 'warning' : ''"
-				@click="isFavorite = !isFavorite"
-			>
-				<v-icon class="favorite" :class="{'is-favorite': isFavorite}">
-					mdi-star{{ isFavorite ? '' : '-outline' }}
-				</v-icon>
-			</v-btn>
+			<h1><router-link to="/room/0">{{ area.name }}</router-link></h1>
 		</div>
 		<div class="card-content">
-			<p>Something about this room</p>
+			<p>{{ area.description }}</p>
 		</div>
 		<div class="card-extras">
 			<o-space-extras />
@@ -47,6 +37,7 @@ const isFavorite = ref(Math.random() > 0.7)
 .space-card {
 	display: grid;
 	grid-template-columns: auto 1fr;
+	grid-template-rows: auto 1fr;
 	gap: 8px 16px;
 	align-items: center;
 
@@ -59,6 +50,8 @@ const isFavorite = ref(Math.random() > 0.7)
 
 	.card-content {
 		grid-column: 2;
+
+		word-break: break-word;
 	}
 
 	.card-extras {
---
+++ front-end/src/plugins/Area.js
@@ -0,0 +1,14 @@
+import { BACKEND_URL } from "./config";
+
+const ALL_AREAS_URL = BACKEND_URL + "/area"
+
+const DEFAULT_BODY = {
+	method: "GET"
+}
+
+export default {
+	async getAreaPagination(page) {
+		return fetch(ALL_AREAS_URL + `?page=${page}`)
+			.then(response => response.json())
+	}
+}
