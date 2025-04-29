005b890813e5506f0889190fb1d69ff8269efb67
Odin Lyngsgård <odin24@outlook.com>
2025-04-29 12:16:47 +0200

    Create SecurityConfiguration.java

---
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/security/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package no.ntnu.idata2900.group3.chairspace.security;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Security configuration class for the application.
+ */
+@Configuration
+public class SecurityConfiguration {
+	
+}
