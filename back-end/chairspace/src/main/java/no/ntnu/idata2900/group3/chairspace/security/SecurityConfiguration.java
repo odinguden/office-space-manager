722bca7271efdfa384f5e5e06b8715504a758b60
Odin Lyngsgård <odinlyngsgard01@gmail.com>
2025-04-29 14:06:54 +0200

    Security configuration now only allows unauthorized users to acess api docs and login endpoints

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/security/SecurityConfiguration.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/security/SecurityConfiguration.java
@@ -1,11 +1,39 @@
 package no.ntnu.idata2900.group3.chairspace.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 /**
  * Security configuration class for the application.
  */
 @Configuration
 public class SecurityConfiguration {
-	
+
+	/**
+	 * Security and sutch.
+	 *
+	 * @param http
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/login").permitAll() // Allow all to access login endpoint
+				.requestMatchers("/api-docs/**").permitAll() // TODO Remove later
+				.anyRequest().authenticated()
+			)
+			.exceptionHandling(e -> e
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)) // Return 401 for unauthorized requests
+			)
+			.oauth2Login(oauth2 -> oauth2
+			.loginPage("/login") //set login endpoint
+			);
+			return http.build();
+	}
 }
