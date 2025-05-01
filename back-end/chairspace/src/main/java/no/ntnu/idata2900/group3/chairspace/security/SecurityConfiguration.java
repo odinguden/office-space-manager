6d3f63528933e5a792e22580eca05955316bf9a9
Odin Lyngsgård <odin24@outlook.com>
2025-05-01 14:48:40 +0200

    Basic session based security config

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/security/SecurityConfiguration.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/security/SecurityConfiguration.java
@@ -1,15 +1,19 @@
 package no.ntnu.idata2900.group3.chairspace.security;
 
 import no.ntnu.idata2900.group3.chairspace.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 /**
  * Security configuration class for the application.
@@ -18,7 +22,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+	@Value("${frontend.url}")
+	private String frontendUrl;
+
 	UserService userService;
+
 
 	/**
 	 * Creates new instance of security configuration.
@@ -32,9 +40,13 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> auth
 				// Allow all to access login endpoint
 				.requestMatchers("/login").permitAll()
+				// Allow all to access microsoft login endpoint
+				.requestMatchers("/oauth2/authorization/azure").permitAll()
 				// TODO Remove later
 				.requestMatchers("/api-docs/**").permitAll()
 				// TODO Remove later
@@ -43,15 +55,24 @@ public class SecurityConfiguration {
 				.requestMatchers("/v3/api-docs/**").permitAll()
 				.anyRequest().authenticated()
 			)
+			// A flaw of this logic is that until the user is logged in,
+			// All errors will return 401. So if debugging, check the logs regularly.
+			.exceptionHandling(exception -> exception
+				.authenticationEntryPoint(
+					new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+				)
+			)
 			.oauth2Login(oauth2 -> oauth2
 				// .loginPage() Forces the user to user microsoft login.
 				// If the application is extended to support other login methods
 				// this should be changed.
 				.loginPage("/oauth2/authorization/azure")
+				.defaultSuccessUrl(frontendUrl, true)
 				.userInfoEndpoint(userInfo -> userInfo
 					.oidcUserService(oidcUserService()
 				)
-			));
+			)
+			);
 		return http.build();
 	}
 
