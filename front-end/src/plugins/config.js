79ae217f8b885e9ae0199da645f6b48f7b7a862f
sigvebjo <sigve@bjorkedal.org>
2025-05-05 13:26:33 +0200

    Added basic user card with logout button

--- front-end/src/plugins/config.js
+++ front-end/src/plugins/config.js
@@ -1,2 +1,3 @@
 export const BACKEND_URL = "http://localhost:8080"
-export const LOGIN_ENDPOINT = "/oauth2/authorization/azure"
+export const LOGIN_ENDPOINT = "/login"
+export const LOGOUT_ENDPOINT = "/logout"
