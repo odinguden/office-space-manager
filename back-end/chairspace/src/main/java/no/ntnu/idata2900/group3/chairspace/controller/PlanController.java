444c3a7748f3933db9ff4dc8eee083c85f58ce9c
Odin Lyngsgård <odin24@outlook.com>
2025-05-06 16:32:00 +0200

    Now able to create plans based on simplePlan

--- back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/controller/PlanController.java
+++ back-end/chairspace/src/main/java/no/ntnu/idata2900/group3/chairspace/controller/PlanController.java
@@ -5,13 +5,18 @@ import java.util.UUID;
 import no.ntnu.idata2900.group3.chairspace.assembler.PlanAssembler;
 import no.ntnu.idata2900.group3.chairspace.dto.SimplePlan;
 import no.ntnu.idata2900.group3.chairspace.entity.Plan;
+import no.ntnu.idata2900.group3.chairspace.exceptions.InvalidArgumentCheckedException;
 import no.ntnu.idata2900.group3.chairspace.service.PlanService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 
 
 /**
@@ -52,5 +57,24 @@ public class PlanController extends AbstractController<Plan, UUID> {
 			.map(planAssembler::toSimple)
 			.toList();
 		return new ResponseEntity<>(simplePlans, HttpStatus.OK);
+	}
+
+	/**
+	 * Creates a new plan based on a simple plan.
+	 *
+	 * @param plan the simple plan to use as base
+	 * @return 200 OK if new plan is created
+	 * @throws ResponseStatusException 400 bad request if values for simple plan is invalid
+	 */
+	@PostMapping("")
+	public ResponseEntity<String> postMethodName(@RequestBody SimplePlan plan) {
+		Plan realPlan;
+		try {
+			realPlan = planAssembler.toDomain(plan);
+		} catch (InvalidArgumentCheckedException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		planService.create(realPlan);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
